4c460949e531d481dcafc7876bd207795dd3492b
Eivind Magnus Nørsterud Hvidevold <hvidevold@gmail.com>
2025-01-18 22:52:37 +0100

    test performance upgrades

--- cuda_test.cu
+++ cuda_test.cu
@@ -6,10 +6,76 @@
 #include <cstdint>
 #include <cmath>
 
-#define CACHE_SIZE (1ULL << 30)  // 1 GiB Cache Size
-#define BASE_BITS 37             // Set to 37 for larger ranges
+#define CACHE_SIZE (1ULL << 2)  // 1 GiB Cache Size
+#define BASE_BITS 36             // Set to 37 for larger ranges
+#define BASE_TABLE_BITS 24
 #define CHUNK_SIZE (1ULL << 30)  // 1 GiB of numbers per chunk
 // #define MAX_ITERATIONS 1024
+
+std::string uint128_to_string(__uint128_t value) {
+    // Split the 128-bit integer into two 64-bit parts
+    const __uint128_t base = 10;
+    std::string result;
+    do {
+        __uint128_t remainder = value % base;
+        value /= base;
+        result = static_cast<char>('0' + remainder) + result;
+    } while (value != 0);
+    return result;
+}
+
+void fill_table_item(uint64_t *B_table, uint64_t *C_table, uint64_t nL, int base_bits) {
+    __uint128_t b = static_cast<__uint128_t>(1) << base_bits; // Start with b = 2^BASE_BITS
+    __uint128_t c = nL;
+
+    while (b % 2 == 0) {
+        if (b % 2 == 0 && c % 2 == 0) {
+            b /= 2;
+            c /= 2;
+        } else if (c % 2 == 1) {
+            b *= 3;
+            c = 3 * c + 1;
+        }
+
+        if (b <= ((static_cast<__uint128_t>(1) << base_bits) - 1)) {
+
+            if (b >= ((__uint128_t) 1 << (__uint128_t) 64)) {
+                // std::cout << "Overflow detected in b: " << uint128_to_string(b) << std::endl;
+                printf("Overflow detected in b\n");
+            }
+
+            if (c >= ((__uint128_t) 1 << (__uint128_t) 64)) {
+                // std::cout << "Overflow detected in c: " << uint128_to_string(c) << std::endl;
+                printf("Overflow detected in c\n");
+            }
+
+            B_table[nL] = (uint64_t) b;
+            C_table[nL] = (uint64_t) c;
+
+            return;
+        }
+    }
+
+    if (b >= ((__uint128_t) 1 << (__uint128_t) 64)) {
+        // std::cout << "Overflow detected in b: " << uint128_to_string(b) << std::endl;
+        printf("Overflow detected in b\n");
+    }
+
+    if (c >= ((__uint128_t) 1 << (__uint128_t) 64)) {
+        // std::cout << "Overflow detected in c: " << uint128_to_string(c) << std::endl;
+        printf("Overflow detected in c\n");
+    }
+
+    B_table[nL] = (uint64_t) b;
+    C_table[nL] = (uint64_t) c;
+}
+
+void fill_table(uint64_t *B_table, uint64_t *C_table, int base_bits) {
+    const uint64_t size = 1 << base_bits;
+    for (uint64_t i = 0; i < size; ++i) {
+        fill_table_item(B_table, C_table, i, base_bits);
+    }
+}
 
 __device__ bool is_mandatory(uint64_t nL, int base_bits) {
     __uint128_t b = static_cast<__uint128_t>(1) << base_bits; // Start with b = 2^BASE_BITS
@@ -28,6 +94,7 @@ __device__ bool is_mandatory(uint64_t nL, int base_bits) {
             return false;
         }
     }
+
     return true;
 }
 
@@ -86,7 +153,7 @@ __device__ int count_trailing_zeros_64(uint64_t n) {
     return (n == 0) ? 64 : __ffsll(n) - 1;
 }
 
-__global__ void convergence_test_iterative(uint64_t *results, uint64_t *powers_of_3, int *cache, uint64_t *S_table, int S_size, uint64_t n_start, uint64_t max_unfiltered) {
+__global__ void convergence_test_iterative(uint64_t *results, uint64_t *powers_of_3, int *cache, uint64_t *B_table, uint64_t *C_table, uint64_t *S_table, int S_size, uint64_t n_start, uint64_t max_unfiltered) {
     uint64_t idx = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (idx >= S_size) return;
@@ -95,14 +162,14 @@ __global__ void convergence_test_iterative(uint64_t *results, uint64_t *powers_o
     __uint128_t n = (static_cast<__uint128_t>(n_start) << BASE_BITS) + mL;
 
     uint64_t n0 = static_cast<uint64_t>(n);
-    int delay = 0;
-    unsigned int iteration_count = 0;
+    // int delay = 0;
+    // unsigned int iteration_count = 0;
 
-    while (n > 1) {
-        if (n < CACHE_SIZE && cache[static_cast<uint64_t>(n)] != -1) {
-            delay += cache[static_cast<uint64_t>(n)];
-            break;
-        }
+    while (n >= n0) {
+        // if (n < CACHE_SIZE && cache[static_cast<uint64_t>(n)] != -1) {
+        //     delay += cache[static_cast<uint64_t>(n)];
+        //     break;
+        // }
 
         // if (iteration_count >= MAX_ITERATIONS) {
         //     printf("Exceeded maximum iterations\n");
@@ -111,20 +178,28 @@ __global__ void convergence_test_iterative(uint64_t *results, uint64_t *powers_o
 
         n = n + 1;
         int a = count_trailing_zeros_64(static_cast<uint64_t>(n));
-        n >>= a;
-        n *= powers_of_3[a];
-        n = n - 1;
-        int b = count_trailing_zeros_64(static_cast<uint64_t>(n));
-        n >>= b;
-
-        delay += a + b;
-        iteration_count++;
+        if (false && a >= BASE_TABLE_BITS) {
+            n >>= a;
+            n *= powers_of_3[a];
+            n = n - 1;
+            int b = count_trailing_zeros_64(static_cast<uint64_t>(n));
+            n >>= b;
+        } else {
+            const uint64_t mask = (1ULL << BASE_TABLE_BITS) - 1; // Mask for BASE_TABLE_BITS
+            const uint64_t nL = n & mask;                           // Extract LSB
+            const uint64_t nH = (n >> (64 - BASE_TABLE_BITS)) & mask; // Extract MSB
+            const uint64_t b = B_table[nL];
+            const uint64_t c = C_table[nL];
+            n = b * nH + c;
+        }
+        // delay += a + b;
+        // iteration_count++;
     }
 
-    if (n0 < CACHE_SIZE) {
-        cache[n0] = delay;
-    }
-    results[idx] = delay;
+    // if (n0 < CACHE_SIZE) {
+    //     cache[n0] = delay;
+    // }
+    // results[idx] = delay;
 }
 
 void initialize_powers_of_3(uint64_t *powers_of_3_host, int max_power) {
@@ -138,6 +213,9 @@ int main() {
     const int base_bits = BASE_BITS;
 
     // Generate S table using GPU in chunks
+    uint64_t BC_size = 1ULL << BASE_TABLE_BITS;
+    uint64_t* B_table = new uint64_t[BC_size];
+    uint64_t* C_table = new uint64_t[BC_size];
     auto start_S = std::chrono::high_resolution_clock::now();
     auto S = generate_S_on_gpu(base_bits);
     auto end_S = std::chrono::high_resolution_clock::now();
@@ -145,6 +223,14 @@ int main() {
 
     std::cout << "Generated S table with size: " << S.size() << " in "
               << elapsed_S.count() << " seconds." << std::endl;
+
+    auto start_BC = std::chrono::high_resolution_clock::now();
+    fill_table(B_table, C_table, BASE_TABLE_BITS);
+    auto end_BC = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_BC = end_BC - start_BC;
+
+    std::cout << "Generated BC table with size: " << BC_size << " in "
+              << elapsed_BC.count() << " seconds." << std::endl;
 
     const uint64_t max_unfiltered = static_cast<uint64_t>(1) << base_bits;
 
@@ -161,15 +247,21 @@ int main() {
     uint64_t *powers_of_3_device;
     int *cache_device;
     uint64_t *S_device;
+    uint64_t *B_device;
+    uint64_t *C_device;
 
     cudaMalloc(&results_device, S.size() * sizeof(uint64_t));
     cudaMalloc(&powers_of_3_device, 65 * sizeof(uint64_t));
     cudaMalloc(&cache_device, CACHE_SIZE * sizeof(int));
     cudaMalloc(&S_device, S.size() * sizeof(uint64_t));
+    cudaMalloc(&B_device, BC_size * sizeof(uint64_t));
+    cudaMalloc(&C_device, BC_size * sizeof(uint64_t));
 
     cudaMemcpy(powers_of_3_device, powers_of_3_host, 65 * sizeof(uint64_t), cudaMemcpyHostToDevice);
     cudaMemcpy(cache_device, cache_host, CACHE_SIZE * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(S_device, S.data(), S.size() * sizeof(uint64_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(B_device, B_table, BC_size * sizeof(uint64_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(C_device, C_table, BC_size * sizeof(uint64_t), cudaMemcpyHostToDevice);
 
     // Start timing
     auto start = std::chrono::high_resolution_clock::now();
@@ -178,7 +270,7 @@ int main() {
     const int threads_per_block = 256;
     const int num_blocks = (S.size() + threads_per_block - 1) / threads_per_block;
 
-    convergence_test_iterative<<<num_blocks, threads_per_block>>>(results_device, powers_of_3_device, cache_device, S_device, S.size(), 1, max_unfiltered);
+    convergence_test_iterative<<<num_blocks, threads_per_block>>>(results_device, powers_of_3_device, cache_device, B_device, C_device, S_device, S.size(), 1, max_unfiltered);
     cudaDeviceSynchronize();
 
     // Stop timing
